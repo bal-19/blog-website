38bfa5099c3dd4b3b02ef63ae93f08819c30d1e9
bal-19 <iqbalhaidee19@gmail.com>
2024-09-12 11:31:41 +0700

    add image field to table

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -12,6 +12,7 @@ return new class extends Migration {
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
+            $table->binary('image');
             $table->string('name');
             $table->string('username')->unique();
             $table->string('email')->unique();
